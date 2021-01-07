5830c32d33cea305142ef54f72d177d05593977b
mochuangang <275064128@qq.com>
2021-01-07 23:13:32 +0800

    scala study

---
+++ src/main/scala/com/bigdata/scala/chapter01/Comment.scala
@@ -0,0 +1,28 @@
+package com.bigdata.scala.chapter01
+
+/**
+ * @author Mocg
+ *
+ */
+object Comment {
+    def main(args: Array[String]): Unit = {
+        println("hello world")
+
+        sum(10, 20)
+    }
+
+    //tab 或者是tab+shift 整体移动
+    /**
+     * @deprecated 过期
+     * @example
+     * 输入n1 = 10 n2 = 20 return 30
+     * @param n1
+     * @param n2
+     * @return 和
+     */
+    def sum(n1: Int, n2: Int): Int = {
+        var str = "hell0" + "hell0" + "hello" + "hello"+ "hello"+ "hello"+ "hello"+ "hello"+ "hello"+ "hello"+ "hello"+ "hello"
+
+        return n1 + n2
+    }
+}
